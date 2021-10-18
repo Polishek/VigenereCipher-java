6cb8a8ce088e433ea219fb8d485a292b44183cf6
Polishek <bartusdera@gmail.com>
2021-10-19 01:20:42 +0200

    Komentarze VigenereModel

--- vigenere-cipher/src/main/java/pl/polsl/vigenere/cipher/VigenereModel.java
+++ vigenere-cipher/src/main/java/pl/polsl/vigenere/cipher/VigenereModel.java
@@ -14,37 +14,52 @@ public class VigenereModel {
     private String secretLetter;
     private String textToCode;
     
+    /**
+     * Constructor of VigenereModel
+     * @param secretLetter Letter given by user to be first in the encoded message
+     * @param textToCode String to be encoded
+     */
     public VigenereModel(String secretLetter, String textToCode){
         this.secretLetter=secretLetter.toUpperCase();
         this.textToCode=textToCode.toUpperCase();
     }
     
+    /**
+     * Method for creating key used to encode message.
+     */
     private void createKey(){
         key = secretLetter + removeLastChar(textToCode);
     }
     
+    /**
+     * Method responsible for encoding given message
+     */
     public void encodeMessage(){  //does it have to be public?
         char[] charEncodedMessage = new char[textToCode.length()];
-        //char[] charTextToCode=textToCode.toCharArray();
-        //char[] charKey=key.toCharArray();
         
         char[][] cipherTable=new char[26][26];
         createCipherTable(cipherTable);
         
-         for(int i = 0; i < encodedMessage.length(); i++) {
-            charEncodedMessage[i] = cipherTable[textToCode.charAt(i)][key.charAt(i)]; // looking for wanted 'encoded' character
+         for(int i = 0; i < textToCode.length(); i++) {
+            charEncodedMessage[i] = cipherTable[textToCode.charAt(i)][key.charAt(i)]; // ERROR: not sure why, maybe char[]=char[][] ? or indexes of cipherTable
         }
         
-        encodedMessage = charEncodedMessage.toString();
+        //encodedMessage = charEncodedMessage.toString();
         System.out.println(encodedMessage);
     }
     
+    /**
+     * Get the atribute encodedMessage
+     * @return Encoded message
+     */
     public String getEncodedMessage(){
         return encodedMessage;
     }
     
     /** 
-     * Method to remove last character
+     * Method to remove last character from given String
+     * @param s String to have last character removed
+     * @return substring os String without last character
      */
     private String removeLastChar(String s){  
         return s.substring(0, s.length() - 1);  
@@ -52,7 +67,7 @@ public class VigenereModel {
     
     /**
      * Initialize cipherTable with uppercase letters
-     * @param cipherTable table to bo initialized
+     * @param cipherTable Table to bo initialized
      */
     private void createCipherTable(char[][] cipherTable){
         int temp;
